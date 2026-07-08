f4ce6c2e18f58242ae155bf20804681c3a5717e6
duyhung2404 <dinhduyhungalt@gmail.com>
2026-07-08 17:16:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+function resize(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resize();
+
+window.addEventListener("resize", resize);
+
+ctx.fillStyle = "red";
+ctx.font = "50px Arial";
+ctx.fillText("❤️",100,100);
