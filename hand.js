78dbdc8f01bf000dc3013d0d32e2d8f79cb8e807
duyhung2404 <dinhduyhungalt@gmail.com>
2026-07-08 17:32:05 +0700

    Update hand.js

--- hand.js
+++ hand.js
@@ -1,1 +1,52 @@
+const video = document.getElementById("video");
 
+
+const hands = new Hands({
+    locateFile:(file)=>{
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }
+});
+
+
+hands.setOptions({
+
+    maxNumHands:1,
+
+    modelComplexity:1,
+
+    minDetectionConfidence:0.7,
+
+    minTrackingConfidence:0.7
+
+});
+
+
+hands.onResults(results=>{
+
+    if(results.multiHandLandmarks){
+
+        console.log("Đã thấy tay!");
+
+    }
+
+});
+
+
+
+const camera = new Camera(video,{
+
+    onFrame: async()=>{
+
+        await hands.send({
+            image:video
+        });
+
+    },
+
+    width:1280,
+    height:720
+
+});
+
+
+camera.start();
