ab6247e5d72acf8ea5364bc182d02c7bd989a844
duyhung2404 <dinhduyhungalt@gmail.com>
2026-07-08 17:28:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+
 function resize(){
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
 }
 
 resize();
 
-window.addEventListener("resize", resize);
+window.addEventListener(
+    "resize",
+    resize
+);
 
-ctx.fillStyle = "red";
-ctx.font = "50px Arial";
-ctx.fillText("❤️",100,100);
+
+
+const hearts=[];
+
+
+for(let i=0;i<500;i++){
+
+    hearts.push(
+        new Heart(canvas)
+    );
+
+}
+
+
+
+function animate(){
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    ctx.fillStyle="#ff3366";
+
+
+    for(let heart of hearts){
+
+        heart.update();
+        heart.draw(ctx);
+
+    }
+
+
+    requestAnimationFrame(animate);
+
+}
+
+
+animate();
